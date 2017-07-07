1c548024b08b77258bb639cdeb43a489c31c4918
Keveren Freeman <freemankeveren@gmail.com>
2017-07-07 14:50:42 -0600

    styling to bucket list

--- client/src/components/BucketList.js
+++ client/src/components/BucketList.js
@@ -4,10 +4,25 @@ import { connect } from 'react-redux';
 import { Header, Button, Segment, Form } from 'semantic-ui-react';
 
 class BucketList extends Component {
+  state = { bucketList: '' };
+  // state that return or false
+  // function to run when button is clicked to update state
+  //
   render() {
     return(
       <Segment basic>
         <Header as='h1' textAlign='center'>Bucket List</Header>
+        <Segment basic textAlign='center'>
+          <button className="ui huge button, teal button" color='teal'>Add Desire</button>
+            <Form.Field>
+              <label>Language</label>
+              <input
+                required
+                placeholder='Enter Wish list'
+                value={this.state.bucketList}
+              />
+            </Form.Field>
+        </Segment>
       </Segment>
     )
   }
