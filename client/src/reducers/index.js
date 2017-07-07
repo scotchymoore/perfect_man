a6e1cb748f63f4c0c6aa8913403536eeff111950
Josie Thompson <josiecthompson@gmail.com>
2017-07-07 16:52:21 -0600

    dateActivity reducer started

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,16 @@ import user from './user'
 import flash from './flash'
 import bucketLists from './bucketList'
 import food from './food'
+import photos from './photos'
+import dateActivity from './dateActivity'
 
 const rootReducer = combineReducers({
   user,
   flash,
   bucketLists,
   food,
+  photos,
+  dateActivity
 })
 
 export default rootReducer
