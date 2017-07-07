790b549864c90775cab82e403042a90548084d96
Alan Vincent <alan.d.vincent@gmail.com>
2017-07-07 16:50:47 -0600

    filled in date activity form

---
+++ client/src/components/DateActivityForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addDateActivity? } from '../actions/dateActions?';
+
+class DateActivityForm extends React.Component {
+    state = { location: '', activity: '' }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const dateActivity= this.state;
+        this.props.dispatch(addDateActivity?(dateActivity));
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target
+        this.setState({ [name]: value });
+    }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          name = "location"
+          placeholder="date location"
+          value = {this.state.location}
+          onChange ={this.handleChange}
+          />
+        <input
+          name = "activity"
+          placeholder = "date activity"
+          value = {this.state.activity}
+          onChange ={this.handleInputChange}
+          />
+        
+      </form>
+    );
+  }
+}
+export default connect()(DateForm);
--- client/src/components/DateForm.js
+++
@@ -1,1 +0,0 @@
-//add date form here
