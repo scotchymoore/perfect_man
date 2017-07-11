4b93047e3e5e4da8179c07817c8b724ecdc2bad1
Keveren Freeman <freemankeveren@gmail.com>
2017-07-11 16:15:04 -0600

    some design on login register page

--- client/src/components/HomeIndex.js
+++ client/src/components/HomeIndex.js
@@ -3,6 +3,15 @@ import { connect } from 'react-redux'
 import { Header, Image, Divider, Segment, Grid, Menu, Message, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+const style = {
+  loginMenu: {
+      height: 'auto',
+      borderWidth: '2px',
+      borderRadius: '5px',
+      margin: '0 auto',
+    }
+}
+
 class HomeIndex extends Component {
 
   handleClick = () => {
@@ -11,13 +20,24 @@ class HomeIndex extends Component {
 
   render() {
     return(
-      <Segment basic>
-        <Header as='h1' textAlign='center'>Wingman</Header>
-        <Button.Group size='large'>
-          <Link to="/Login"><Button>Login</Button></Link>
-            <Button.Or />
-          <Link to="/Register"><Button>Register</Button></Link>
-        </Button.Group>
+      <Segment basic textAlign='center'>
+      <Header as='h1' textAlign='center'>Wingman</Header>
+        <Grid centered width={30}>
+          <Grid.Column streched>
+            <Segment basic textAlign='center'>
+              <Menu vertical
+                style={style.loginMenu}
+                >
+
+              <Button.Group size='large'>
+                <Link to="/Login"><Button>Login</Button></Link>
+                  <Button.Or />
+                <Link to="/Register"><Button>Register</Button></Link>
+              </Button.Group>
+            </Menu>
+              </Segment>
+          </Grid.Column>
+        </Grid>
       </Segment>
     )
   }
