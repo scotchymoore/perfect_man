62f81fde458838cfab6ee088715a9b0aa855eae2
Scott Moore <scotchymoore@gmail.com>
2017-07-12 13:24:38 -0600

    background image

--- client/src/components/HomeIndex.js
+++ client/src/components/HomeIndex.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Header, Image, Divider, Segment, Grid, Menu, Message, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import backgroundImage from '../assets/black-diamond-plate.jpg';
+
+
+
 
 class HomeIndex extends Component {
 
@@ -11,16 +15,35 @@ class HomeIndex extends Component {
 
   render() {
     return(
-      <Segment basic>
+
+      <Segment basic style={styles.main}>
         <Header as='h1' textAlign='center'>Wingman</Header>
         <Button.Group size='large'>
           <Link to="/Login"><Button>Login</Button></Link>
             <Button.Or />
           <Link to="/Register"><Button>Register</Button></Link>
         </Button.Group>
+
       </Segment>
+
     )
   }
+}
+
+const styles = {
+  main: {
+    height: 640,
+    width: null,
+    background: `url(${backgroundImage}) no-repeat center center fixed`,
+    webkitBackgroundSize: 'cover',
+    mozBackgroundSize: 'cover',
+    oBackgroundSize: 'cover',
+    backgroundSize: 'cover',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
 }
 
 const mapStateToProps = (state) => {
